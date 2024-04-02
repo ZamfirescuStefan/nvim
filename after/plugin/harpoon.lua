33346be1a618aa6e6d70c22ebc8f649da6779c68
stefanzamfirescu <zamfirescu.stefan12@gmail.com>
2024-04-02 03:03:38 +0300

    Remove useless keymap for harpoon

--- after/plugin/harpoon.lua
+++ after/plugin/harpoon.lua
@@ -24,7 +24,6 @@ harpoon:extend({
 vim.keymap.set('n', '<leader>a', function() harpoon:list():append() end)
 vim.keymap.set('n', '<leader>m', function() harpoon:list():remove() end)
 vim.keymap.set('n', '<leader>e', function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
-vim.keymap.set('n', '<leader>hm', function() harpoon:list():clear() end)
 
 
 vim.keymap.set('n', '<leader>1', function() harpoon:list():select(1) end)
@@ -35,6 +34,7 @@ vim.keymap.set('n', '<leader>5', function() harpoon:list():select(5) end)
 vim.keymap.set('n', '<leader>6', function() harpoon:list():select(6) end)
 vim.keymap.set('n', '<leader>7', function() harpoon:list():select(7) end)
 vim.keymap.set('n', '<leader>8', function() harpoon:list():select(8) end)
+vim.keymap.set('n', '<leader>9', function() harpoon:list():select(9) end)
 
 
 vim.keymap.set('n', '<C-S-P>', function() harpoon:list():prev() end)
