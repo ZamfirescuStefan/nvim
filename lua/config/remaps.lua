58908681b3d06174e04062b723a9009b7c25fef7
stefanzamfirescu <zamfirescu.stefan12@gmail.com>
2024-06-17 23:15:48 +0300

    Small refactoring

--- lua/config/remaps.lua
+++ lua/config/remaps.lua
@@ -42,11 +42,4 @@ vim.keymap.set('n', '<leader>cp', '<cmd>let @+ = expand(\"%:p\")<CR>', { desc = 
 vim.keymap.set('v', '<', '<gv', opts)
 vim.keymap.set('v', '>', '>gv', opts)
 
--- Highlight when yanking
-vim.api.nvim_create_autocmd('TextYankPost', {
-    desc = 'Highlight when yanking (copying) text',
-    group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
-    callback = function()
-        vim.highlight.on_yank()
-    end,
-})
+
--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,5 +1,7 @@
 return {
     'marko-cerovac/material.nvim',
+    lazy = false,
+    priority = 1000,
     config = function ()
         vim.g.material_style = 'deep ocean'
         color = color or 'material'
