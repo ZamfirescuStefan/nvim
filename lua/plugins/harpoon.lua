7ebf1d0e2adcdbc9a2dc90f33cf64eb339032079
stefanzamfirescu <zamfirescu.stefan12@gmail.com>
2025-02-22 17:32:19 +0200

    Update nvim packages

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -29,7 +29,7 @@ return {
             end,
         })
 
-        vim.keymap.set('n', '<leader>a', function() harpoon:list():append() end)
+        vim.keymap.set('n', '<leader>a', function() harpoon:list():add() end)
         vim.keymap.set('n', '<leader>h', function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
 
