936ae248f53b0ca1d1f4074bbacc02a0895406b6
stefanzamfirescu <zamfirescu.stefan12@gmail.com>
2025-05-21 14:36:16 +0300

    Added markdown plugin

---
+++ lua/plugins/markdown.lua
@@ -0,0 +1,5 @@
+return {
+    'MeanderingProgrammer/render-markdown.nvim',
+    dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' , 'echasnovski/mini.icons'}, -- if you use the mini.nvim suite
+    opts = {},
+}
